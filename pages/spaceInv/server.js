5bfe030adfaf757ba11e07fea5597b05cbc8df62
Derpytitan64 <74693803+Derpytitan64@users.noreply.github.com>
2025-06-24 17:03:47 +1000

    Update server.js

--- pages/spaceInv/server.js
+++ pages/spaceInv/server.js
@@ -38,3 +38,57 @@ let copyOfShipBody = {
   position: "",
   velocity: "",
 };
+const realtime = Ably.Realtime({
+  key: ABLY_API_KEY,
+  echoMessages: false,
+});
+
+//create a uniqueId to assign to clients on auth
+const uniqueId = function () {
+  return "id-" + totalPlayers + Math.random().toString(36).substr(2, 16);
+};
+
+app.use(express.static("js"));
+
+app.get("/auth", (request, response) => {
+  const tokenParams = { clientId: uniqueId() };
+  realtime.auth.createTokenRequest(tokenParams, function (err, tokenRequest) {
+    if (err) {
+      response
+        .status(500)
+        .send("Error requesting token: " + JSON.stringify(err));
+    } else {
+      response.setHeader("Content-Type", "application/json");
+      response.send(JSON.stringify(tokenRequest));
+    }
+  });
+});
+
+app.get("/", (request, response) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (++peopleAccessingTheWebsite > MIN_PLAYERS_TO_START_GAME) {
+    response.sendFile(__dirname + "/views/gameRoomFull.html");
+  } else {
+    response.sendFile(__dirname + "/views/intro.html");
+  }
+});
+
+app.get("/gameplay", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/winner", (request, response) => {
+  response.sendFile(__dirname + "/views/winner.html");
+});
+
+app.get("/gameover", (request, response) => {
+  response.sendFile(__dirname + "/views/gameover.html");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
